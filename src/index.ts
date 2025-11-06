1b9ca33f4480a8b15f1d31169f1e29e4e5474258
MCODE OPS <mcode-bot@modelcode.ai>
2025-11-06 00:25:40 +0000

    task-m4920-t5210-Milestone_1-Task_1_verification

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * LetHimCook - A TypeScript library for unit conversions, especially for cooking
+ */
+
+// This is a placeholder file to allow the TypeScript compiler to run successfully.
+// Actual exports will be added as modules are migrated.
+
+export {};
